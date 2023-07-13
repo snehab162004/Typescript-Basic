5fd6b092375c507a245cf4ae641e9eee4d1d5b9f
Sneha B~ <bsneha.162004@gmail.com>
2023-07-12 23:12:21 -0700

    enum concept done

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
-let studName = 'a';
-console.log(studName);
-studName = 'sneha';
-console.log(studName);
+let studName;
+let age;
+let isRanked;
+let unkown;
+var Color;
+(function (Color) {
+    Color["red"] = "red";
+    Color["blue"] = "blue";
+    Color["black"] = "black";
+    Color["yellow"] = "yellow";
+})(Color || (Color = {}));
+function getColor(color) {
+    console.log({ color });
+}
+getColor(Color.red);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-let studName = 'a';
-console.log(studName);
 
-studName = 'sneha';
 
-console.log(studName);
+let studName : string
+let age : number
+let isRanked : boolean
+let unkown : null | undefined
+
+enum Color {
+    red = 'red',
+    blue = 'blue',
+    black = 'black',
+    yellow = 'yellow'
+}
+
+
+
+function getColor (color: Color) {
+    console.log({ color })
+}
+
+getColor(Color.red)
